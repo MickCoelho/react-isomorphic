fcd8cc310281ff2eb69ab154a9a905a3582f85e6
Mickael <mickael@rehabstudio.com>
2015-04-14 11:38:54 +0100

    Updated Android address bar color

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -21,6 +21,7 @@ var Head = React.createClass( {
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
                 <meta name="apple-mobile-web-app-capable" content="yes"/>
                 <meta name="mobile-web-app-capable" content="yes"/>
+                <meta name="theme-color" content="#c2185b"/>
             </head>
         );
     }
